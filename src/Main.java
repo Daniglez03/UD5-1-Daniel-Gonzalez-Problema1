3e82ced0b70738912af2d30c9be1e97f69732d4c
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-05-05 10:26:06 +0100

    Featured: Commit de mantenimiento

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        GenerickQueue<Float> colita = new GenerickQueue<Float>();
+        boolean entradaUsuario = false;
+        while (!entradaUsuario) {
+            System.out.println("1.......Introducir valor");
+            System.out.println("Q.......Sumar todo y salir");
 
+            System.out.println("Qué deseas hacer?");
+            String opcion = sc.nextLine();
+            switch (opcion) {
+                case"1":
+                    System.out.println("Añade un número");
+                    float valorIntroducido = sc.nextFloat();
+                    colita.enqueue(valorIntroducido);
+                    break;
+                case"Q":
+                    Float suma = 0f;
+                    for (int i = 0; i < colita.numObjetos; i++) {
+                        suma += colita[i];
+                    }
+                    System.out.println(suma);
+                    System.out.println("Saliendo...");
+                    entradaUsuario = true;
+                    break;
+                default:
+                    System.out.println("Introduce una opción correcta...");
+            }
+        }
     }
 }
